22b9ea9c2a607ab68b2cd39df9278fd1d67dbc01
Will Fisher <retrochicken.dev@gmail.com>
2019-05-15 09:38:10 -0500

    Added a type hierarchy

--- src/main/java/poulet/typing/Checker.java
+++ src/main/java/poulet/typing/Checker.java
@@ -58,7 +58,9 @@ public class Checker {
             Abstraction abstraction = (Abstraction) term;
             Expression abstractionType = Interpreter.evaluateExpression(abstraction.type).expression();
 
-            checkKind(context, abstractionType);
+            // TODO: Make check kind the same as deduce Type
+            //checkKind(context, abstractionType);
+            deduceType(context, abstractionType);
 
             Context newContext = context.increment();
             Symbol tempSymbol = Util.getUniqueSymbol();
@@ -88,14 +90,30 @@ public class Checker {
                     if(application.function instanceof PiType) {
                         PiType piType = (PiType) application.function;
                         checkType(context, application.argument, piType.type);
-                        result = new Variable(new Symbol("Type1"));
+                        result = new Variable(new Symbol(name));
                     }
                 }
             } else
                 throw new TypeException("Application Function is not a Function");
         } else if (term instanceof PiType) {
-            checkKind(context, term);
-            result = new Variable(new Symbol("Type1"));
+            //checkKind(context, term);
+            PiType piType = (PiType) term;
+            Expression typeLevel = deduceType(context, piType.type);
+            Symbol uniqueSymbol = Util.getUniqueSymbol();
+            Context newContext = context.increment();
+            newContext = newContext.append(uniqueSymbol, Interpreter.evaluateExpression(piType.type).expression());
+            Expression bodyLevel = deduceType(newContext, substitute(piType.body, new Variable(uniqueSymbol)));
+            if(typeLevel instanceof Variable && bodyLevel instanceof Variable) {
+                Variable typeVar = (Variable) typeLevel;
+                Variable bodyVar = (Variable) bodyLevel;
+                String typeName = typeVar.symbol.getName();
+                String bodyName = bodyVar.symbol.getName();
+                if(typeName.matches("Type\\d+") && bodyName.matches("Type\\d+")) {
+                    int level1 = Integer.parseInt(typeName.substring(4));
+                    int level2 = Integer.parseInt(bodyName.substring(4));
+                    result = new Variable(new Symbol("Type" + umax(level1, level2)));
+                }
+            }
         }
         if(result == null)
             throw new TypeException("Type Could not be Deduced");
@@ -137,5 +155,10 @@ public class Checker {
         }
 
         return base;
+    }
+
+    private static int umax(int level1, int level2) {
+        if(level1 <= 2 && level2 <= 2) return 1;
+        return Math.max(level1, level2);
     }
 }
